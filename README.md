5654be70f2b4e8b1f4ccfab901309240e8074bd5
Vivek Bhatt <vivek.bhatt@krishtechnolabs.com>
2021-07-04 15:27:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Documentation for Magento PWA Studio packages is located at [https://pwastudio.io](https://pwastudio.io).
+
+     - cd pwa before running the below commands.
+
+     - yarn run buildpack create-custom-origin . to generate a unique, secure custom domain for your new project. Highly recommended.
+
+     - yarn run watch to start the dev server and do real-time development.
+
+     - yarn run storybook to start Storybook dev server and view available components in your app.
+
+     - yarn run build to build the project into optimized assets in the '/dist' directory.
+
+     - yarn start after build to preview the app on a local staging server.
+
---
+++ local-intercept.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+/**
+ * Custom interceptors for the project.
+ *
+ * This project has a section in its package.json:
+ *    "pwa-studio": {
+ *        "targets": {
+ *            "intercept": "./local-intercept.js"
+ *        }
+ *    }
+ *
+ * This instructs Buildpack to invoke this file during the intercept phase,
+ * as the very last intercept to run.
+ *
+ * A project can intercept targets from any of its dependencies. In a project
+ * with many customizations, this function would tap those targets and add
+ * or modify functionality from its dependencies.
+ */
+
+function localIntercept(targets) {
+    targets.of("@magento/venia-ui").routes.tap(routes => {
+    routes.push({
+      name: "MyGreetingRoute",
+      pattern: "/greeting/:who?",
+      path: require.resolve("./src/components/GreetingPage/greetingPage.js")
+    });
+    return routes;
+  });
+}
+
+module.exports = localIntercept;
+
+// module.exports = targets => {
+//   targets.of("@magento/venia-ui").routes.tap(routes => {
+//     routes.push({
+//       name: "MyGreetingRoute",
+//       pattern: "/greeting/:who?",
+//       path: require.resolve("./src/components/GreetingPage/greetingPage.js")
+//     });
+//     return routes;
+//   });
+// };
